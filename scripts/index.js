82eee1be24cebd0cbd701f15b5f48ec83a214bb0
Vladislav Skulachenko <vladik-skulahenko@yandex.ru>
2025-04-05 22:27:00 +0300

    Add new logic in index,js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const deleteCard = function () {
+  const removingCard = this.closest('.card');
+  removingCard.remove();
+};
 
-// @todo: DOM узлы
+function cardCreate(cardData, deleteCard) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = card.querySelector('.card__image');
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  const cardDescription = card.querySelector('.card__description');
+  cardDescription.textContent = cardData.name;
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard);
+  return card;
+}
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach((element) => {
+  const newCard = cardCreate(element, deleteCard);
+  placesList.append(newCard);
+});
